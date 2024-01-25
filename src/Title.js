99329ad030be6af50c5dcf4853b8feef4b15bd22
airjlee <airjlee@cs.washington.edu>
2024-01-24 21:13:48 -0800

    temp

--- src/Title.js
+++ src/Title.js
@@ -4,8 +4,8 @@ import './Title.css';
 const Title = () => {
   return (
     <div className="title-container">
-      <h1 className="title">TWONOUNS</h1>
-      <h2 className="subtitle">CLOTHING BRAND</h2>
+      <h1 className="title">I LOVE CHLOE LEE</h1>
+      <h2 className="subtitle">she's beautiful</h2>
     </div>
   );
 };

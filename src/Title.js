3add1eadcb2c61ea013e9ad9bddf063a5acd0f8a
airjlee <airjlee@cs.washington.edu>
2024-01-28 03:47:15 -0800

    added korean to title

--- src/Title.js
+++ src/Title.js
@@ -6,6 +6,7 @@ const Title = () => {
     <div className="title-container">
       <h1 className="title">TWONOUNS</h1>
       <h2 className="subtitle">CLOTHING BRAND</h2>
+      <h3 className="korean">옷이 날개다</h3>
     </div>
   );
 };

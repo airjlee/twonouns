f372901fd65660a539a6dece80d05666f24152f5
airjlee <airjlee@cs.washington.edu>
2024-02-08 15:36:48 -0800

    fixed require bug

--- src/BackgroundVideo.js
+++ src/BackgroundVideo.js
@@ -10,7 +10,7 @@ const BackgroundVideo = () => {
     return (
       <div >
         <img
-          src={require('bloodlilyimg.png')}
+          src={require('./bloodlilyimg.png')}
           alt="Background"
           className="background-image"
         />

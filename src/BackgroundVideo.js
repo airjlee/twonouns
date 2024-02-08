40299829aa962491a80fb84e80d0db60a903fe1c
airjlee <airjlee@cs.washington.edu>
2024-02-08 15:24:46 -0800

    build mobile background

--- src/BackgroundVideo.js
+++ src/BackgroundVideo.js
@@ -10,7 +10,7 @@ const BackgroundVideo = () => {
     return (
       <div >
         <img
-          src="bloodlilyimg.png"
+          src="./bloodlilyimg.png"
           alt="Background"
           className="background-image"
         />

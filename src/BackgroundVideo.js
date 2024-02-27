9f6437e42063e52be2db3c8ddeaf037fcc09fd76
airjlee <airjlee@cs.washington.edu>
2024-02-27 13:49:10 -0800

    mobile functionality and minor tweaks

--- src/BackgroundVideo.js
+++ src/BackgroundVideo.js
@@ -4,21 +4,17 @@ import './BackgroundVideo.css';
 
 const BackgroundVideo = () => {
 
-  /*
+  
   const isMobile = window.innerWidth <= 768; // Adjust the threshold as needed
 
   if (isMobile) {
     return (
-      <div >
-        <img
-          src={require('./bloodlilyimg.png')}
-          alt="Background"
-          className="background-image"
-        />
-      </div>
+      <><div class="youtube-container-mobile">
+      <iframe src="https://www.youtube.com/embed/5WCpGEnv9po?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=5WCpGEnv9po" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div></>
     );
   }
-*/
+
   
   
   return (
--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import './Footer.css'; // Import CSS file for styling
+import React, { useRef, useEffect, useState } from 'react';
+import './Footer.css';
+import 'animate.css';
 
 const Footer = () => {
+  const emailRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.5 } 
+    );
+
+    if (emailRef.current) {
+      observer.observe(emailRef.current);
+    }
+
+
+    return () => {
+      if (emailRef.current) {
+        observer.unobserve(emailRef.current);
+      }
+    };
+  }, []);
+
   return (
     <footer className="footer">
       <div className="email-container">
-        <input type="email" placeholder="Enter your email" />
+        <div className={`animate__animated ${isVisible ? 'animate__fadeIn' : ''}`} ref={emailRef}>
+            <div className="footer-subtitle">JOIN THE WAITLIST</div>
+          <input type="email" placeholder="Enter your email" />
+        </div>
       </div>
       <div className="footer-header">
-            TWONOUNS | 2024 ©
+        TWONOUNS | 2024 ©
       </div>
- 
     </footer>
   );
 };

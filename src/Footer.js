0214f3c4f7aab7af579563c190279c4122f0fdeb
airjlee <airjlee@cs.washington.edu>
2024-02-27 19:01:16 -0800

    email functionality

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,35 @@ import 'animate.css';
 const Footer = () => {
   const emailRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+  const [isJoined, setIsJoined] = useState(false); // State to track if user has joined
+  const [email, setEmail] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(true); // State to track if email is valid
+
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  const handleEmailChange = (e) => {
+    const inputEmail = e.target.value;
+    setEmail(inputEmail);
+    // Clear the error message when user starts typing
+    setIsValidEmail(true);
+  };
+
+  const handleStayUpdated = () => {
+    // Validate email
+    setIsValidEmail(validateEmail(email));
+    if (validateEmail(email)) {
+      setIsJoined(true); // Update state to indicate user has joined
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleStayUpdated();
+    }
+  };
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -31,14 +60,27 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="email-container">
-        <div className={`animate__animated ${isVisible ? 'animate__fadeIn' : ''}`} ref={emailRef}>
-            <div className="footer-subtitle">JOIN THE WAITLIST</div>
-          <input type="email" placeholder="Enter your email" />
+      {isJoined ? (
+            <text className="thanks">Thank you for joining the waitlist.</text>
+          ) : (
+        <div className={`${isVisible ? 'animate__animated animate__fadeIn' : 'black'}`} ref={emailRef}>
+          <div className="footer-subtitle">JOIN THE WAITLIST</div>
+          <input type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={handleEmailChange}
+                onKeyPress={handleKeyPress}
+           />
+          {!isValidEmail && <text className="invalid-email-msg">Please enter a valid email address.</text>}
+        </div>
+          )}
+      </div>
+      <div className='bottom'>
+        <div className="footer-header">
+          TWONOUNS | 2024 ©
         </div>
       </div>
-      <div className="footer-header">
-        TWONOUNS | 2024 ©
-      </div>
+      
     </footer>
   );
 };

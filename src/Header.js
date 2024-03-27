f76a37bf10fe531413d152b160f6bcea292eedf9
airjlee <airjlee@cs.washington.edu>
2024-03-26 20:10:13 -0700

    made header sticky

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,9 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='collection'>
-      <div>SUMMER SPRING 24 </div>
-      <div>7.17.2024 </div>
-      <div>WEAR YOUR WINGS</div>
+      <div style={{position: "fixed"}}>SUMMER SPRING 24 </div>
+      <div style={{position: "fixed", left: "48.3%"}}>7.17.2024 </div>
+      <div style={{position: "fixed", left: "90.4%"}}>WEAR YOUR WINGS</div>
     </div>
   );
 };

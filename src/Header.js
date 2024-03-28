c8a4ef4ca818e11b00ccd4dc792fcd91e95e0cb3
airjlee <airjlee@cs.washington.edu>
2024-03-27 17:23:05 -0700

    added scroll image previews

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 
 const Header = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className='collection'>
-      <div style={{position: "fixed"}}>SUMMER SPRING 24 </div>
-      <div style={{position: "fixed", left: "48.3%"}}>7.17.2024 </div>
-      <div style={{position: "fixed", left: "90.4%"}}>WEAR YOUR WINGS</div>
+      <div className='left'>SUMMER SPRING 24 </div>
+      <div className='center'>7.17.2024 </div>
+       <div className='right'>WEAR YOUR WINGS</div>
+     
+      
     </div>
   );
 };
--- src/Title.js
+++ src/Title.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Title.css';
-import { useRef, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import 'animate.css';
 import WebFont from 'webfontloader';
-  
+import BloodLily from './blood_lily.png';
+import DaisyBloom from './blood_lily_better.png'
+import DoveWings from './dove_Wings.png'
 
 const Title = () => {
   useEffect(() => {
@@ -14,8 +16,8 @@ const Title = () => {
       },
     });
   }, []);
-  const emailRef = useRef(null);
-  const [isVisible, setIsVisible] = useState(false);
+
+  const [scrollPosition, setScrollPosition] = useState(0);
   const [isJoined, setIsJoined] = useState(false); // State to track if user has joined
   const [email, setEmail] = useState('');
   const [isValidEmail, setIsValidEmail] = useState(true); // State to track if email is valid
@@ -49,31 +51,91 @@ const Title = () => {
     }
   };
 
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setIsVisible(true);
-        }
-      },
-      { rootMargin: '-500px 0px 0px 0px', } 
-    );
-
-    if (emailRef.current) {
-      observer.observe(emailRef.current);
-    }
-
-
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      if (emailRef.current) {
-        observer.unobserve(emailRef.current);
-      }
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+  const calculateOpacity = () => {
+    const maxScroll = 700; // Adjust as needed
+    const opacity = 1 - (scrollPosition / maxScroll);
+    return opacity < 0 ? 0 : opacity;
+  };
+
+  const calculateOpacityTwo = () => {
+    const fadeInStart = 800;
+    const fadeInEnd = 1500;
+    const fadeOutStart = 1500;
+    const fadeOutEnd = 2200;
+  
+    if (scrollPosition < fadeInStart) {
+      return 0; // Opacity is 0 before the fadeInStart
+    } else if (scrollPosition >= fadeInStart && scrollPosition < fadeInEnd) {
+      // Opacity increases linearly from 0 to 1 between fadeInStart and fadeInEnd
+      return (scrollPosition - fadeInStart) / (fadeInEnd - fadeInStart);
+    } else if (scrollPosition >= fadeInEnd && scrollPosition < fadeOutStart) {
+      return 1; // Opacity is 1 between fadeInEnd and fadeOutStart
+    } else if (scrollPosition >= fadeOutStart && scrollPosition < fadeOutEnd) {
+      // Opacity decreases linearly from 1 to 0 between fadeOutStart and fadeOutEnd
+      return 1 - (scrollPosition - fadeOutStart) / (fadeOutEnd - fadeOutStart);
+    } else {
+      return 0; // Opacity is 0 after fadeOutEnd
+    }
+  };
+
+  const calculateOpacityThree = () => {
+    const fadeInStart = 2300;
+    const fadeInEnd = 3000;
+    const fadeOutStart = 3000;
+    const fadeOutEnd = 3700;
+  
+    if (scrollPosition < fadeInStart) {
+      return 0; // Opacity is 0 before the fadeInStart
+    } else if (scrollPosition >= fadeInStart && scrollPosition < fadeInEnd) {
+      // Opacity increases linearly from 0 to 1 between fadeInStart and fadeInEnd
+      return (scrollPosition - fadeInStart) / (fadeInEnd - fadeInStart);
+    } else if (scrollPosition >= fadeInEnd && scrollPosition < fadeOutStart) {
+      return 1; // Opacity is 1 between fadeInEnd and fadeOutStart
+    } else if (scrollPosition >= fadeOutStart && scrollPosition < fadeOutEnd) {
+      // Opacity decreases linearly from 1 to 0 between fadeOutStart and fadeOutEnd
+      return 1 - (scrollPosition - fadeOutStart) / (fadeOutEnd - fadeOutStart);
+    } else {
+      return 0; // Opacity is 0 after fadeOutEnd
+    }
+  };
+
+  const calculateOpacityFour = () => {
+    const fadeInStart = 3800;
+    const fadeInEnd = 4500;
+    const fadeOutStart = 4500;
+    const fadeOutEnd = 5200;
+  
+    if (scrollPosition < fadeInStart) {
+      return 0; // Opacity is 0 before the fadeInStart
+    } else if (scrollPosition >= fadeInStart && scrollPosition < fadeInEnd) {
+      // Opacity increases linearly from 0 to 1 between fadeInStart and fadeInEnd
+      return (scrollPosition - fadeInStart) / (fadeInEnd - fadeInStart);
+    } else if (scrollPosition >= fadeInEnd && scrollPosition < fadeOutStart) {
+      return 1; // Opacity is 1 between fadeInEnd and fadeOutStart
+    } else if (scrollPosition >= fadeOutStart && scrollPosition < fadeOutEnd) {
+      // Opacity decreases linearly from 1 to 0 between fadeOutStart and fadeOutEnd
+      return 1 - (scrollPosition - fadeOutStart) / (fadeOutEnd - fadeOutStart);
+    } else {
+      return 0; // Opacity is 0 after fadeOutEnd
+    }
+  };
+
   return (
     <div className='animate__animated animate__fadeIn'>
-        <div className="title-container">
+       {scrollPosition < 800 && (
+      <div className="title-container" style={{ opacity: calculateOpacity() }} >
       <h1 className={
 
         `${isMobile ? `${isValidEmail ? 'mobile-title' : 'mobile-title'}` : `${isValidEmail ? 'title' : 'title'}`} `
@@ -82,6 +144,28 @@ const Title = () => {
       <h2 className="subtitle">DESIGN STUDIO</h2>
       <h3 className="korean">옷이 날개다</h3>
       </div>
+       )}
+
+       {scrollPosition > 799 &&(
+         <div className="title-container" style={{ opacity: calculateOpacityTwo() }} >
+          <img src={DoveWings}/>
+            <h2 className="label">01 / DOVE WINGS</h2>
+            
+         </div>
+       )}
+
+        
+         <div className="title-container" style={{ opacity: calculateOpacityThree() }} >
+            <img className="prev" src={DaisyBloom}/>
+            <h2 className="label">02 / DAISY BLOOM</h2>
+         </div>
+       
+
+         <div className="title-container" style={{ opacity: calculateOpacityFour() }} >
+         <img src={BloodLily}/>
+            <h2 className="label">03 / BLOOD LILY</h2>
+         </div>
+
       <div className="email-container">
         
         <div className='footer-container'>
